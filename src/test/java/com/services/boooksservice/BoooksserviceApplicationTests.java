24e103c69155277fae46f36338cc99ca21afeb5c
Chandan Kumar <chandan.btech9@gmail.com>
2020-11-11 12:12:51 +0530

    initial commit

---
+++ src/test/java/com/services/boooksservice/BoooksserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.boooksservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoooksserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
